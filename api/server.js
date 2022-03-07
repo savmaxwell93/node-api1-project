31108629475da23b4c3c4c71d4f3eb75eaababe4
Savannah <savmaxwell@gmail.com>
2022-03-07 13:10:15 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -6,4 +6,26 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+    console.log('Create new user!')
+})
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The users information could not be retrieved"})
+        })
+})
+server.get('/api/users/:id', (req, res) => {
+    console.log('Get one user by id!')
+})
+server.delete('/api/users/:id', (req, res) => {
+    console.log('Delete user by id!')
+})
+server.put('/api/users/:id', (req, res) => {
+    console.log('Update user by id!')
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
